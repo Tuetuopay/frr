e4b9232209ac0fa2667d9145672cfadae12d66dc
Tuetuopay <tuetuopay@me.com>
2025-04-25 18:28:41 +0200

    bgpd: evpn: properly handle exported route attribute update
    Signed-off-by: Tuetuopay <tuetuopay@me.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -5009,7 +5009,7 @@ void bgp_update(struct peer *peer, const struct prefix *p, uint32_t addpath_id,
 	struct bgp_path_info *new = NULL;
 	const char *reason;
 	char pfx_buf[BGP_PRD_PATH_STRLEN];
-	bool force_evpn_import = false;
+	bool force_evpn_import = false, force_evpn_export = false;
 	safi_t orig_safi = safi;
 	struct bgp_labels bgp_labels = {};
 	struct bgp_route_evpn *p_evpn = evpn;
@@ -5498,9 +5498,11 @@ void bgp_update(struct peer *peer, const struct prefix *p, uint32_t addpath_id,
 		}
 #endif
 
-		if (advertise_type5_routes(bgp, afi) && is_route_injectable_into_evpn(pi))
+		if (advertise_type5_routes(bgp, afi) && is_route_injectable_into_evpn(pi)) {
 			/* Implicit withdraw case */
 			bgp_evpn_unexport_type5_route(bgp, dest, pi, afi, safi);
+			force_evpn_export = true;
+		}
 
 		/* Special handling for EVPN update of an existing route. If the
 		 * extended community attribute has changed, we need to
@@ -5571,6 +5573,12 @@ void bgp_update(struct peer *peer, const struct prefix *p, uint32_t addpath_id,
 			}
 		}
 #endif
+
+		if (advertise_type5_routes(bgp, afi) && is_route_injectable_into_evpn(pi)
+		    && force_evpn_export) {
+			/* Add back the route with its new attributes (e.g. local-pref). */
+			bgp_evpn_export_type5_route(bgp, dest, pi, afi, safi);
+		}
 
 		/* Update bgp route dampening information.  */
 		if (get_active_bdc_from_pi(pi, afi, safi) &&
@@ -5654,10 +5662,6 @@ void bgp_update(struct peer *peer, const struct prefix *p, uint32_t addpath_id,
 		    && (bgp->inst_type == BGP_INSTANCE_TYPE_DEFAULT)) {
 			vpn_leak_to_vrf_update(bgp, pi, prd, peer);
 		}
-
-		if (advertise_type5_routes(bgp, afi) && is_route_injectable_into_evpn(pi))
-			/* Implicit withdraw case */
-			bgp_evpn_unexport_type5_route(bgp, dest, pi, afi, safi);
 
 #ifdef ENABLE_BGP_VNC
 		if (SAFI_MPLS_VPN == safi) {
